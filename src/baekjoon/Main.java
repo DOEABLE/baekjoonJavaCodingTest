a38711c705e3febb4e93878cb4fd7ef4b10f980f
DOEABLE <kimdh090588@gmail.com>
2025-07-09 01:00:16 +0900

    이진탐색구현

---
+++ src/baekjoon/Main.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		int[] arr = {1, 3, 5, 7, 9, 11, 13};
+		System.out.println(binarySearch(arr, 9));
+	}
+
+	public static int binarySearch(int[] arr, int target) {
+		int left = 0;
+		int right = arr.length - 1;
+
+		int count = 0;
+		while (left <= right) {
+
+			int mid = (left + right) / 2;
+
+			if (arr[mid] == target) {
+				return mid;
+			} else if (arr[mid] > target) {
+				right = mid - 1;
+				count++;
+			} else {
+				left = mid + 1;
+				count++;
+			}
+		}
+		return count;
+	}
+}
+
+
